7911da5fc70f3950d018419d19cdcf7d530d4eb7
Jon-Schneider <jon@jonschneider.me>
2017-06-28 20:22:31 -0700

    Replaces Owncloud with Nextcloud.

--- me.sh
+++ me.sh
@@ -49,7 +49,7 @@ function install_casks {
 	brew cask install libreoffice
 	brew cask install macdown
 	# brew cask install minecraft
-	brew cask install owncloud
+	brew cask install nextcloud
 	brew cask install qlmarkdown
 	brew cask install qlmobi
 	brew cask install rescuetime
