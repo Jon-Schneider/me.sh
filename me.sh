cc3e6b833d29c397ffa994de10a174d2adae7aa5
Jon-Schneider <jon@jonschneider.me>
2017-10-06 18:15:10 -0700

    Adds epub-quicklook to casks.

--- me.sh
+++ me.sh
@@ -44,6 +44,7 @@ function install_casks {
 	brew cask install cyberduck
 	brew cask install deploymate
 	brew cask install disk-inventory-x
+	brew cask install epubquicklook
 	brew cask install flux
 	brew cask install google-chrome
 	# brew cask install kindle
