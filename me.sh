369dacda830f52aaa955ba0fd6b064520c8dc896
Jon Schneider <jsc@microsoft.com>
2020-10-14 09:26:16 -0700

    Adds onedrive cask to me.sh

--- me.sh
+++ me.sh
@@ -79,7 +79,7 @@ brew tap caskroom/cask
 message "Finished Installing Homebrew Cask"
 
 message "Installing Casks"
-brew cask install bbedit calibre coconutbattery cryptomator dash disk-inventory-x drawio epubquicklook flux karabiner-elements kindle kitty ksdiff libreoffice macdown microsoft-edge rectangle qlmarkdown qlmobi the-unarchiver torbrowser visual-studio-code
+brew cask install bbedit calibre coconutbattery cryptomator dash disk-inventory-x drawio epubquicklook flux karabiner-elements kindle kitty ksdiff libreoffice macdown microsoft-edge onedrive rectangle qlmarkdown qlmobi the-unarchiver torbrowser visual-studio-code
 brew tap buo/cask-upgrade
 message "Finished Installing Casks"
 
