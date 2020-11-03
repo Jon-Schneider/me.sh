cb2e0ba7c23864bb4aadb4f314fc0ce369e6be6d
Jon Schneider <jsc@microsoft.com>
2020-11-03 10:03:44 -0800

    Remove gopath config from me.sh; Adds more logs to me.sh for fun

--- me.sh
+++ me.sh
@@ -89,69 +89,83 @@ defaults write com.barebones.bbedit SoftWrapStyle -integer 2
 defaults write com.barebones.bbedit EditingWindowShowPageGuide -bool NO
 
 # Configure OS to show hidden files in Finder
+message "Configuring Finder to show hidden files..."
 defaults write com.apple.Finder AppleShowAllFiles true
 killall Finder
 
 # Create Tmp Dir
+message "Creating ~/Tmp dir..."
 mkdir ~/Tmp
 
 # Set Screenshot Save Location
+message "Setting screenshot save location..."
 defaults write com.apple.screencapture location ~/Tmp
 killall SystemUIServer
 
 # Disable Annoying Screenshot Previews
+message "Disabling annoying screenshot previews..."
 defaults write com.apple.screencapture show-thumbnail -bool TRUE
 
 # Copy Visual Studio Code config files to default location
+message "Copying Visual Studio Code config..."
 cp -v vscode/keybindings.json ~/Library/Application\ Support/Code/User/
 cp -v vscode/settings.json ~/Library/Application\ Support/Code/User/
 
-# Set up .vimrc and .vim
+# Configure vom
+message "Configuring Vim..."
 mkdir -p ~/.vim/.backup ~/.vim/.tmp ~/.vim/.undo
 cp .vimrc ~/
 
 # Install Oh My Zsh
+message "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Copy .zshrc
+message "Copying .zshrc..."
 cp -v .zshrc ~/.zshrc
 
 # Copy Kitty .conf
+message "Copying Kitty Config..."
 mkdir -p ~/.config/kitty
 cp -v kitty.conf ~/.config/kitty/
 
 # Add '~/bin' to bash PATH
+message "Adding ~/bin to PATH..."
 sed -i '' -e '$a\' ~/.bash_profile && echo export "PATH=\"\$PATH:\$HOME/bin\"" >> ~/.bash_profile
 
-# Set bash $GOPATH
-sed -i '' -e '$a\' ~/.bash_profile && echo export "export GOPATH=\"$HOME/go\"" >> ~/.bash_profile
-
 # Set up Karabiner configuration
+message "Configuring Karabiner Elements..."
 mkdir ~/.config
 cp -vR karabiner ~/.config
 
 # Copy Rectangle Config
+message "Copying Rectangle config..."
 cp com.knollsoft.Rectangle.plist ~/Library/Preferences/
 
 # Crank up the key repeat rates and trackpad speed. We've got stuff to do.
 # You will have to log out for these preferences to be applied
+message "Configuring Keyboard and Trackpad..."
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 defaults write -g com.apple.mouse.scaling 6.0 # Double the default
 
 # Configure git
+message "Configuring git..."
 cp ./.gitignore ~/.gitignore_global # Copy global .gitignore
 cp -v .gitconfig ~/.gitconfig # Copy .gitconfig
 
 # Configure Login Script
+message "Setting up login script..."
 cp -v login/login.sh ~/
 cp -v login/com.user.loginscript.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/com.user.loginscript.plist
 
 # Copy .lldbinit
+message "Copying lldb config..."
 cp -v .lldbinit ~/
 
 # Set default apps
+message "Setting default apps..."
 duti -s com.barebones.bbedit public.plain-text all # All plaintext files. Includes Unix hidden files
 duti -s com.barebones.bbedit json all
 duti -s com.barebones.bbedit sh all
@@ -162,6 +176,7 @@ duti -s com.microsoft.edgemac http # Set Edge as default browser
 # Theming
 # Override system theme to set certain apps to always display in dark or light mode.
 # To reset run 'defaults delete [bundleid] NSRequiresAquaSystemAppearance'
+message "Overriding system theme in specific apps..."
 defaults write com.microsoft.onenote.mac NSRequiresAquaSystemAppearance -bool no # OneNote should always be dark mode
 
 message "Configuration Complete"
