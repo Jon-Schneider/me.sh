4b963146f76b9699fb018029d228d38e1bcaf53d
Jon Schneider <jon@jonschneider.me>
2020-11-10 14:51:30 -0800

    Disables unnecessary Lunette keybindings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,19 @@
-hs.loadSpoon("Lunette")
-spoon.Lunette:bindHotkeys()
-
 -- Defeat Pasteblocking
 hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
+-- Disable Lunette bindings I don't need
+hs.loadSpoon("Lunette")
+windowManagementBindings = {
+    center = false,
+    topLeft = false,
+    bottomLeft = false,
+    topRight = false,
+    bottomRight = false,
+    nextThird = false,
+    prevThird = false,
+    enlarge = false,
+    shrink = false,
+    undo = false,
+    redo = false
+}
+spoon.Lunette:bindHotkeys(windowManagementBindings)
