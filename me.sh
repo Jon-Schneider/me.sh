fe0c8ed60e5f465e817c451da805c58381e88246
Jon Schneider <jon@jonschneider.me>
2020-11-11 23:26:42 -0800

    Adds a bunch of config changes from donnemartin/dev-setup

--- me.sh
+++ me.sh
@@ -82,7 +82,19 @@ defaults write com.barebones.bbedit EditingWindowShowPageGuide -bool NO
 # Configure Finder
 message "Configuring Finder ..."
 defaults write com.apple.Finder AppleShowAllFiles true
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/OneDrive/"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false # Disable the warning when changing a file extension
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv" # Use Column View by default
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Expand the Get Info “General”, “Open with”, and “Sharing & Permissions” File Info panes by default:
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+    General -bool true \
+    OpenWith -bool true \
+    Privileges -bool true
+	
 killall Finder
 
 # Create Tmp Dir
@@ -167,18 +179,61 @@ duti -s com.barebones.bbedit txt all
 duti -s com.uranusjr.macdown md all
 duti -s com.microsoft.edgemac http # Set Edge as default browser
 
-# Disable Aidrop by default
-defaults write com.apple.NetworkBrowser DisableAirDrop -bool YES
-
-# Customize Statusbar
-defaults write com.apple.Siri StatusMenuVisible NO
-defaults write com.apple.systemuiserver menuExtras '("/System/Library/CoreServices/Menu Extras/Battery.menu", "/System/Library/CoreServices/Menu Extras/Bluetooth.menu", "/System/Library/CoreServices/Menu Extras/Clock.menu")'
-killall SystemUIServer
-
-# Theming
 # Override system theme to set certain apps to always display in dark or light mode.
 # To reset run 'defaults delete [bundleid] NSRequiresAquaSystemAppearance'
 message "Overriding system theme in specific apps..."
 defaults write com.microsoft.onenote.mac NSRequiresAquaSystemAppearance -bool no # OneNote should always be dark mode
+
+message "Misc. Config..."
+
+# Disable Aidrop by default
+defaults write com.apple.NetworkBrowser DisableAirDrop -bool YES
+
+# Hide Siri in Statusbar
+defaults write com.apple.Siri StatusMenuVisible NO
+
+# Hide Time Machine, Volume, User icon in Statusbar
+for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
+    defaults write "${domain}" dontAutoLoad -array \
+        "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+        "/System/Library/CoreServices/Menu Extras/Volume.menu" \
+        "/System/Library/CoreServices/Menu Extras/User.menu"
+done
+defaults write com.apple.systemuiserver menuExtras -array \
+    "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+    "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+    "/System/Library/CoreServices/Menu Extras/Battery.menu" \
+    "/System/Library/CoreServices/Menu Extras/Clock.menu"
+
+# Apply Statusbar Changes
+killall SystemUIServer
+
+#Disable Boot Sound
+sudo nvram SystemAudioVolume=" "
+
+# Dxpand Save Panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Enable MacBook Air SuperDrive
+sudo nvram boot-args="mbasd=1"
+
+# Configure Dock
+defaults write com.apple.dock persistent-apps -array # Remove all icons from dock
+defaults write com.apple.dock autohide -bool true
+
+# Disable the Launchpad gesture (pinch with thumb and three fingers)
+defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
 
 message "Configuration Complete"
