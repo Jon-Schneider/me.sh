ec08a2711f2ece66c76e7804dc3d0e37af4e6dd4
Jon Schneider <jon@jonschneider.me>
2020-11-16 20:01:12 -0800

    Adds 'message' function to top of app and system config scripts. This should really be broke out into a separate file

--- sync_app_config.sh
+++ sync_app_config.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function message {
+	GREEN='\033[0;32m'
+	NOCOLOR='\033[0m'
+	printf "${GREEN}$1${NOCOLOR}\n"
+}
+
 message "Updating Application Configuration..."
 
 # Configure BBEdit 
