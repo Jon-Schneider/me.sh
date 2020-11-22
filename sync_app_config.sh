6bd0841fc1dcb659e3b9b133e0ad45e09038ed90
Jon Schneider <jon@jonschneider.me>
2020-11-22 11:03:31 -0800

    Adds incantations to set Forklift as default mac file viewer from https://binarynights.com/manual\#ptools

--- sync_app_config.sh
+++ sync_app_config.sh
@@ -19,6 +19,8 @@ defaults write com.barebones.bbedit EditingWindowShowPageGuide -bool NO
 
 # Configure Forklift
 ln -f configs/Forklift/com.binarynights.ForkLift-3.plist ~/Library/Preferences
+defaults write -g NSFileViewer -string com.binarynights.ForkLift-3;
+defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType="public.folder";LSHandlerRoleAll="com.binarynights.ForkLift-3";}'
 
 # Configure git
 message "Configuring git..."
