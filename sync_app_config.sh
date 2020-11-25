b3025657c6a3989285504555c3c75788a1bfa23f
Jon Schneider <jon@jonschneider.me>
2020-11-24 22:36:21 -0800

    Moves BBEdit config into 'configs' dir

--- sync_app_config.sh
+++ sync_app_config.sh
@@ -11,7 +11,7 @@ message "Updating Application Configuration..."
 # Configure BBEdit 
 message "Configuring BBEdit..."
 mkdir -p ~/Library/Application\ Support/BBEdit 2> /dev/null # Redirect stderr to suppress dir already exists log
-ln -Fs $(pwd)/bbedit/Setup ~/Library/Application\ Support/BBEdit/ # Have to symlink dir because changes aren't syncing with hard link (ln)
+ln -Fs $(pwd)/configs/bbedit/Setup ~/Library/Application\ Support/BBEdit/ # Have to symlink dir because changes aren't syncing with hard link (ln)
 defaults write com.barebones.bbedit SurfNextPreviousInDisplayOrder -bool YES # Configure BBedit to navigate between tabs in display order, not open order
 defaults write com.barebones.bbedit EditorSoftWrap -bool YES
 defaults write com.barebones.bbedit SoftWrapStyle -integer 2
