27ad570e43a847304120ae2c6652b2adb11b1740
Jon Schneider <jon@jonschneider.me>
2020-11-25 18:44:27 -0800

    Removes Forklift. At the end of the day it's better than Finder in some ways, but the ways I want (I just want it to always display in columns, grouped by kind, sorted by name, with hidden files visible) it's not an improvement.

--- sync_app_config.sh
+++ sync_app_config.sh
@@ -17,11 +17,6 @@ defaults write com.barebones.bbedit EditorSoftWrap -bool YES
 defaults write com.barebones.bbedit SoftWrapStyle -integer 2
 defaults write com.barebones.bbedit EditingWindowShowPageGuide -bool NO
 
-# Configure Forklift
-ln -f configs/Forklift/com.binarynights.ForkLift-3.plist ~/Library/Preferences
-defaults write -g NSFileViewer -string com.binarynights.ForkLift-3;
-defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType="public.folder";LSHandlerRoleAll="com.binarynights.ForkLift-3";}'
-
 # Configure git
 message "Configuring git..."
 ln -f configs/git/.gitignore ~/.gitignore
