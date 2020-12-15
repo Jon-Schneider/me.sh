2abcb4006c6ac04775f897fc21eb564b9b14b5a3
Jon Schneider <jon@jonschneider.me>
2020-12-15 14:17:58 -0800

    Replaces configs/sys/scripts with configs/sys/bin; recreates get_finder_selected_item_paths script because of some sort of permissions issue

---
+++ configs/sys/bin/configure_user_bin.sh
@@ -0,0 +1,2 @@
+echo "Configuring User bin..."
+cp -v configs/sys/bin/get_finder_selected_item_paths ~/bin/
--- configs/sys/scripts/configure_scripts.sh
+++
@@ -1,2 +0,0 @@
-echo "Configuring User Scripts..."
-ln -f configs/sys/scripts/get_finder_selected_item_paths.sh ~/bin/
