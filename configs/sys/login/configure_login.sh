2b856bcfeb584c64d5406ffc13fe6e0d5753e6b8
Jon Schneider <jon@jonschneider.me>
2020-12-15 15:05:04 -0800

    Makes launchtl load of login script sudo

--- configs/sys/login/configure_login.sh
+++ configs/sys/login/configure_login.sh
@@ -3,4 +3,4 @@ ln -f configs/sys/login/login.sh ~/bin/
 mkdir -p ~/Library/LaunchAgents
 sudo cp -v configs/sys/login/com.user.loginscript.plist ~/Library/LaunchAgents # Unfortunately it doesn't appear plists can be linked (ln)
 sudo chown root ~/Library/LaunchAgents/com.user.loginscript.plist
-launchctl load ~/Library/LaunchAgents/com.user.loginscript.plist
+sudo launchctl load ~/Library/LaunchAgents/com.user.loginscript.plist
