8ddada1aa19499d9b09ff3f2cd088ae201734efd
Jon Schneider <jon@jonschneider.me>
2020-12-15 11:55:30 -0800

    Adds 'get_finder_selected_item_paths.sh' to ~/bin

---
+++ configs/sys/scripts/configure_scripts.sh
@@ -0,0 +1,2 @@
+echo "Configuring User Scripts..."
+ln -f configs/sys/scripts/get_finder_selected_item_paths.sh ~/bin/
---
+++ configs/sys/scripts/get_finder_selected_item_paths.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env osascript
+set AppleScript's text item delimiters to linefeed
+
+tell application "Finder"
+   set fSelection to selection as text
+   set thisPOSIXPath to (the POSIX path of fSelection)
+end tell
