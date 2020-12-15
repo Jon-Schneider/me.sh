e0eb74d20d75b2e194a89a243b4ea891810b5458
Jon Schneider <jon@jonschneider.me>
2020-12-15 14:54:24 -0800

    Fixed login script config

--- configs/sys/login/configure_login.sh
+++ configs/sys/login/configure_login.sh
@@ -1,5 +1,6 @@
 echo "Configuring Login Script..."
 ln -f configs/sys/login/login.sh ~/bin/
+mkdir -p ~/Library/LaunchAgents
 sudo cp -v configs/sys/login/com.user.loginscript.plist ~/Library/LaunchAgents # Unfortunately it doesn't appear plists can be linked (ln)
 sudo chown root ~/Library/LaunchAgents/com.user.loginscript.plist
 launchctl load ~/Library/LaunchAgents/com.user.loginscript.plist
