bfef8f938daca58d9893ae3335c6b087a6ed90a1
Jon Schneider <jon@jonschneider.me>
2020-12-17 14:59:31 -0800

    Adds Bracket Pair Colorizer VSCode Extension

--- sync_packages.sh
+++ sync_packages.sh
@@ -15,6 +15,7 @@ brew bundle
 message "Installing Visual Studio Code Extensions"
 code --install-extension Arjun.swagger-viewer
 code --install-extension blanu.vscode-styled-jsx
+code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension eamodio.gitlens
 code --install-extension formulahendry.code-runner
 code --install-extension LaurentTreguier.vscode-simple-icons # I like the 'minimalist' monocrome icons
