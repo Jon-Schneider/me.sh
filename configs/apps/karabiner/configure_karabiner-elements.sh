10f75f1c9e8d37a211e14fcdf6964faded14ab79
Jon Schneider <jon@jonschneider.me>
2021-01-24 12:37:14 -0800

    Karabiner config script updated to create config dir if missing

--- configs/apps/karabiner/configure_karabiner-elements.sh
+++ configs/apps/karabiner/configure_karabiner-elements.sh
@@ -1,3 +1,4 @@
 echo "Configuring Karabiner Elements..."
+mkdir -p ~/.config/karabiner 2> /dev/null # Redirect stderr to suppress dir already exists log
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -f $current_dir/karabiner.json ~/.config/karabiner
