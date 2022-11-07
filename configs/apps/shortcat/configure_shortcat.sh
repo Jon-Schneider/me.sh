b8408ce2b5c30ccd4de019d66f00082d8d0a4363
Jon Schneider <jon@jonschneider.me>
2022-11-07 12:45:28 -0500

    Sets custom Shortcat scroll keys

---
+++ configs/apps/shortcat/configure_shortcat.sh
@@ -0,0 +1,5 @@
+echo "Configuring Shortcat..."
+defaults write com.sproutcube.Shortcat upKeycode 40
+defaults write com.sproutcube.Shortcat downKeycode 38
+defaults write com.sproutcube.Shortcat leftKeycode 41
+defaults write com.sproutcube.Shortcat rightKeycode 39
