e29882d7a0a2da754d0ab2893abc465da134d236
Jon Schneider <jon@jonschneider.me>
2023-01-24 13:34:47 -0500

    Adds Pocket.app icon config

--- configs/sys/Icons/configure_app_icons.sh
+++ configs/sys/Icons/configure_app_icons.sh
@@ -14,6 +14,7 @@ sudo iconsur set /Applications/Gramps.app -s 0.7 -l
 sudo iconsur set /Applications/Horo.app -l
 sudo iconsur set /Applications/Kindle.app
 sudo iconsur set /Applications/Minecraft.app
+sudo iconsur set /Applications/Pocket.app
 sudo iconsur set /Applications/RescueTime.app
 sudo iconsur set /Applications/TorBrowser.app -l
 sudo iconsur set /Applications/VLC.app
