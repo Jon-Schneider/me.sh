7b48c52abf90777aab929b489d806077fa888bf4
Jon Schneider <jon@jonschneider.me>
2024-08-11 11:53:43 -0400

    Implements git diff for apple .strings files

--- configs/apps/git/configure_git.sh
+++ configs/apps/git/configure_git.sh
@@ -4,3 +4,4 @@ ln -f $current_dir/.gitignore ~/.gitignore
 ln -f $current_dir/.gitconfig ~/.gitconfig
 ln -f $current_dir/.gitconfig-js ~/.gitconfig_js
 ln -f $current_dir/.gitconfig-ms ~/.gitconfig_ms
+ln -f $current_dir/.gitattributes ~/.gitattributes
