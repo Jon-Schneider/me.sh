b4569ebe20aab40cce19f161bf48e42593d76d6a
Jon Schneider <jon@jonschneider.me>
2024-08-14 14:33:38 -0400

    Removes custom Cryptomator icon configuration

--- configs/sys/Icons/configure_app_icons.sh
+++ configs/sys/Icons/configure_app_icons.sh
@@ -6,7 +6,6 @@ sudo iconsur set /Applications/calibre.app -l
 sudo iconsur set /Applications/CHIRP.app -l -s 0.8
 sudo iconsur set /Applications/coconutBattery.app -l
 sudo iconsur set /Applications/CubicSDR.app -l
-sudo iconsur set /Applications/Cryptomator.app
 sudo iconsur set /Applications/Dash.app -l
 sudo iconsur set /Applications/Disk\ Inventory\ X.app -l
 sudo iconsur set /Applications/Firefox.app
