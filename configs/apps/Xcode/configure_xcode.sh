781a51d93b75a09c6288b7f89be2b1254d86a65d
Jon Schneider <jon@jonschneider.me>
2025-03-04 11:00:06 -0500

    Adds customizing Xcode keyboard shortcuts to Xcode app configuration

--- configs/apps/Xcode/configure_xcode.sh
+++ configs/apps/Xcode/configure_xcode.sh
@@ -1,4 +1,10 @@
 echo "Configuring Xcode..."
 
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Configure Xcode to display build duration, not finish time
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
+
+# Configure keyboard shortcuts
+# This file cannot be symlinked because Xcode writes a copy when updated
+cp -v $current_dir/Default.idekeybindings /Users/jon/Library/Developer/Xcode/UserData/KeyBindings/Default.idekeybindings
