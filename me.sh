0d8efcb01b87444020eefa08546550b87d4be289
Jon Schneider <jon@jonschneider.me>
2025-05-12 20:53:47 -0400

    Updates Homebrew install incantation

--- me.sh
+++ me.sh
@@ -42,7 +42,7 @@ message "Rosetta 2 Installed"
 
 # Brew
 message "Installing Homebrew"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+- [ ] /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homeberw/bin/brews shellenv)"
 brew update
