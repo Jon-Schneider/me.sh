8909a8d8a2d93d40b094c1b8a2e1029cb2754c19
Jon Schneider <jon@jonschneider.me>
2025-05-21 21:20:28 -0400

    Adds starship shell config

--- configs/apps/shell/configure_shell.sh
+++ configs/apps/shell/configure_shell.sh
@@ -1,8 +1,10 @@
 echo "Configuring .zshrc..."
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p ~/.config/htop 2> /dev/null # Redirect stderr to suppress dir already exists log
 ln -f $current_dir/.zshrc ~/
 ln -f $current_dir/.zsh_plugins ~/
 ln -f $current_dir/.p10k.zsh ~/
+ln -f $current_dir/starship.tom ~/.config
 
 # Setup fastlane completions
 bundle exec fastlane enable_auto_complete
