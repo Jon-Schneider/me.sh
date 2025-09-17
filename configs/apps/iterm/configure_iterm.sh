157ea65a60042ed73b940fcd4b3c709675c9dd57
Jon Schneider <jon@jonschneider.me>
2025-09-17 10:40:26 -0400

    Replaces Kitty terminal with iTerm 2

---
+++ configs/apps/iterm/configure_iterm.sh
@@ -0,0 +1,3 @@
+echo "Configuring iTerm 2..."
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -f $current_dir/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
--- configs/apps/kitty/configure_kitty.sh
+++
@@ -1,4 +0,0 @@
-echo "Configuring Kitty..."
-mkdir -p ~/.config/kitty 2> /dev/null # Redirect stderr to suppress dir already exists log
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -f $current_dir/kitty.conf ~/.config/kitty/
