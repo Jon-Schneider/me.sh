785b3278604dd20997eba65db01bdcffbd375abb
Jon Schneider <jon@jonschneider.me>
2026-01-21 13:13:57 -0500

    Adds hammerspoon keyboard shortcut to paste markdown table template

--- configs/apps/hammerspoon/hammerspoon/init.lua
+++ configs/apps/hammerspoon/hammerspoon/init.lua
@@ -5,6 +5,23 @@ spoon.ReloadConfiguration:start()
 -- Defeat Pasteblocking and paste without retaining formatting
 hs.hotkey.bind({"ctrl", "option", "cmd"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 
+-- Paste Markdown table template I use most often in Chrome or Safari
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", function()
+    local app = hs.application.frontmostApplication()
+    local name = app:name()
+    if name == "Google Chrome" or name == "Safari" then
+        local lines = {
+            "| Before  | After |",
+            "| ------------- | ------------- |",
+            "|  |  |"
+        }
+        for _, line in ipairs(lines) do
+            hs.eventtap.keyStrokes(line)
+            hs.eventtap.keyStroke({}, "return") -- simulate pressing Enter
+        end
+    end
+end)
+
 -- Disable Lunette bindings I don't need
 hs.loadSpoon("Lunette")
 windowManagementBindings = {
