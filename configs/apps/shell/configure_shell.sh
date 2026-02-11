a027dbfaa796923fd8acbf7622302cd4c2691d6c
Jon Schneider <jon@jonschneider.me>
2026-02-11 09:40:14 -0500

    Fixes reference to starship.toml file in configure_shell.sh

--- configs/apps/shell/configure_shell.sh
+++ configs/apps/shell/configure_shell.sh
@@ -3,7 +3,7 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.config/htop 2> /dev/null # Redirect stderr to suppress dir already exists log
 ln -f $current_dir/.zshrc ~/
 ln -f $current_dir/.zsh_plugins ~/
-ln -f $current_dir/starship.tom ~/.config
+ln -f $current_dir/starship.toml ~/.config
 
 # Setup fastlane completions
 bundle exec fastlane enable_auto_complete
