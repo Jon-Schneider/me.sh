87bb28a954f781b1d8eb98d15341a77032d3a934
Jon Schneider <jsc@mini26.local>
2026-04-01 11:00:40 -0400

    Updates BBEdit configuration script to delete preexisting settings dir

--- configs/apps/bbedit/configure_bbedit.sh
+++ configs/apps/bbedit/configure_bbedit.sh
@@ -1,7 +1,10 @@
 echo "Configuring BBEdit..."
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mkdir -p ~/Library/Application\ Support/BBEdit 2> /dev/null # Redirect stderr to suppress dir already exists log
+rm -rf ~/Library/Application\ Support/BBEdit/Setup 2> /dev/null # Need to delete preexisting BBEdit Setup dir if it already exists, redirect stderr to suppress dir already exists log
 ln -Fs $current_dir/Setup ~/Library/Application\ Support/BBEdit/ # Have to symlink dir because changes aren't syncing with hard link (ln)
+
 defaults write com.barebones.bbedit SurfNextPreviousInDisplayOrder -bool YES # Configure BBedit to navigate between tabs in display order, not open order
 defaults write com.barebones.bbedit EditorSoftWrap -bool YES
 defaults write com.barebones.bbedit SoftWrapStyle -integer 2
