6a10f62fc3ba55bbae159598ce61ff2a04c88234
Jon Schneider <jon@jonschneider.me>
2026-06-15 22:06:49 -0400

    Removes custom VLC app icon

--- configs/sys/Icons/configure_app_icons.sh
+++
@@ -1,9 +0,0 @@
-echo "Configuring custom app icons..."
-
-# Apps with iOS equivalant or nice-looking permutations of default icon can be generated with iconsur
-sudo iconsur set /Applications/VLC.app
-
-# Set custom icon images for some applications
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-sudo iconsur cache # Update the system icon cache and reload Finder & Dock
