640520044dbf8290ecb12ca6db48c4410820743e
Jon Schneider <jon@jonschneider.me>
2026-08-18 16:11:34 -0400

    Migrates shell config files to symlinks

--- configs/apps/shell/configure_shell.sh
+++ configs/apps/shell/configure_shell.sh
@@ -1,10 +1,10 @@
 echo "Configuring .zshrc..."
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.config/htop 2> /dev/null # Redirect stderr to suppress dir already exists log
-ln -f $current_dir/.zshrc ~/
-ln -f $current_dir/.zsh_plugins ~/
-ln -f $current_dir/starship.toml ~/.config
-ln -f $current_dir/autoenv.zsh ~/bin
+ln -sfn $current_dir/.zshrc ~/.zshrc
+ln -sfn $current_dir/.zsh_plugins ~/.zsh_plugins
+ln -sfn $current_dir/starship.toml ~/.config/starship.toml
+ln -sfn $current_dir/autoenv.zsh ~/bin/autoenv.zsh
 
 # Setup fastlane completions
 # bundle exec fastlane enable_auto_complete
