8769dac6c787f898f20423175ae5f71bb65a6bb5
Jon Schneider <jon@jonschneider.me>
2026-08-18 12:57:18 -0400

    Deletes iterm2 config

--- configs/apps/iterm/configure_iterm.sh
+++
@@ -1,3 +0,0 @@
-echo "Configuring iTerm 2..."
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -f $current_dir/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
