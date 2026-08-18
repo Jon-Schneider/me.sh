892016550e0b25c9554e113d71d4bf5dad30f208
Jon Schneider <jon@jonschneider.me>
2026-08-18 16:59:15 -0400

    Updates configure_ssh_config.sh to symlink scripts

--- configs/sys/ssh/configure_ssh_config.sh
+++ configs/sys/ssh/configure_ssh_config.sh
@@ -1,1 +1,4 @@
-cp -p configs/sys/ssh/config ~/.ssh/
+echo "Configuring SSH..."
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p ~/.ssh
+ln -sfn $current_dir/config ~/.ssh/config
