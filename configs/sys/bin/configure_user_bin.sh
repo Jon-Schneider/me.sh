533fbbfc243c8629a99b154dd499e00f00046d2e
Jon Schneider <jon@jonschneider.me>
2026-08-18 16:59:01 -0400

    Updates configure_user_bin.sh to symlink scripts

--- configs/sys/bin/configure_user_bin.sh
+++ configs/sys/bin/configure_user_bin.sh
@@ -1,6 +1,7 @@
 echo "Configuring User bin..."
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/bin
-cp -v configs/sys/bin/get_finder_selected_item_paths ~/bin/
-cp -v configs/sys/bin/git_rebase_bbedit ~/bin/
-cp -v configs/sys/bin/starship-github-pr ~/bin/
-cp -v configs/sys/bin/starship-github-pr-refresh ~/bin/
+ln -sfn $current_dir/get_finder_selected_item_paths ~/bin/get_finder_selected_item_paths
+ln -sfn $current_dir/git_rebase_bbedit ~/bin/git_rebase_bbedit
+ln -sfn $current_dir/starship-github-pr ~/bin/starship-github-pr
+ln -sfn $current_dir/starship-github-pr-refresh ~/bin/starship-github-pr-refresh
