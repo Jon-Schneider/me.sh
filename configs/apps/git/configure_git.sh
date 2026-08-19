f35c1e1e3a370a9ece3d85101ea4b75beacca28f
Jon Schneider <jon@jonschneider.me>
2026-08-19 14:37:16 -0400

    Generalizes .gitconfig import system

--- configs/apps/git/configure_git.sh
+++ configs/apps/git/configure_git.sh
@@ -2,7 +2,6 @@ echo "Configuring git..."
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sfn $current_dir/.gitignore ~/.gitignore
 ln -sfn $current_dir/.gitconfig ~/.gitconfig
-ln -sfn $current_dir/.gitconfig-js ~/.gitconfig_js
-ln -sfn $current_dir/.gitconfig-ms ~/.gitconfig_ms
+ln -sfn $current_dir/.gitconfig_jsc ~/.gitconfig_jsc
 ln -sfn $current_dir/.gitattributes ~/.gitattributes
 ln -sfn $current_dir/git-branch-select ~/bin/git-branch-select
