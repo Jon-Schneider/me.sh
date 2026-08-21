fbaa474e5d80f6f918cab4be0ea805649ae37d68
Jon Schneider <jon@jonschneider.me>
2026-08-21 09:56:14 -0400

    Replaces deprecated VS Code extensions, drops dead bundle install

--- sync_packages.sh
+++ sync_packages.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
+set -euo pipefail
 
-function message {
-	GREEN='\033[0;32m'
-	NOCOLOR='\033[0m'
-	printf "${GREEN}$1${NOCOLOR}\n"
-}
+repo_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$repo_root" || exit 1
 
-message "Running 'bundle install'..."
-bundle install
+source "lib/common.sh"
 
 message "Running 'brew bundle'..."
 brew bundle --verbose # Slow so I want verbose output to know something is happening
@@ -22,10 +19,9 @@ code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.go
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension PeterJausovec.vscode-docker
+code --install-extension ms-azuretools.vscode-docker # replaces deprecated PeterJausovec.vscode-docker
 code --install-extension pkief.material-icon-theme
 code --install-extension rangav.vscode-thunder-client
-code --install-extension rebornix.ruby
+code --install-extension shopify.ruby-lsp # replaces unmaintained rebornix.ruby
 code --install-extension redhat.vscode-yaml
 code --install-extension yzhang.markdown-all-in-one
