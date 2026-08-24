8ca275bfc5ff7c240b2deefd96eafa3274778746
Jon Schneider <jon@jonschneider.me>
2026-08-24 17:24:57 -0400

    Migrate Xcode config to manifest

---
+++ configs/apps/Xcode/post.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Configuring Xcode..."
+
+# Configure Xcode to display build duration, not finish time.
+defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
