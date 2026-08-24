ed76b851d6820a9584fe219500d9dd6da362a552
Jon Schneider <jon@jonschneider.me>
2026-08-24 17:23:16 -0400

    Migrate Hammerspoon config to manifest

--- configs/apps/hammerspoon/configure_hammerspoon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Configuring Hammerspoon"
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -fhs $current_dir/hammerspoon ~/.hammerspoon # For some reason this would not work with a relative path, an absolute path was required
