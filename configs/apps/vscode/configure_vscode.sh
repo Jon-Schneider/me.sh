438572a2fd866b12d9bf1472618623d00fa35cdb
Jon Schneider <jon@jonschneider.me>
2026-08-24 17:25:14 -0400

    Migrate VS Code config to manifest

--- configs/apps/vscode/configure_vscode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Configuring Visual Studio Code..."
-mkdir -p ~/Library/Application\ Support/Code/User/ 2> /dev/null # Redirect stderr to suppress dir already exists log
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -sfn $current_dir/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sfn $current_dir/settings.json ~/Library/Application\ Support/Code/User/settings.json
