d364712c7dae603fa9da1d041151c8bf2831fb88
Jon Schneider <jon@jonschneider.me>
2026-08-24 17:23:24 -0400

    Migrate micro config to manifest

--- configs/apps/micro/configure_micro.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Configuring micro..."
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir -p ~/.config/micro 2> /dev/null # Redirect stderr to suppress dir already exists log
-
-ln -sfn $current_dir/bindings.json ~/.config/micro/bindings.json
-ln -sfn $current_dir/settings.json ~/.config/micro/settings.json
