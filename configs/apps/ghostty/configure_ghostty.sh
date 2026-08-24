b855a32b1e82cd62af80fb3bcb9b670a6c6b5895
Jon Schneider <jon@jonschneider.me>
2026-08-24 17:23:12 -0400

    Migrate Ghostty config to manifest

--- configs/apps/ghostty/configure_ghostty.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Configuring Ghostty..."
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir -p ~/.config/ghostty/themes
-
-ln -sfn $current_dir/config ~/.config/ghostty/config
-ln -sfn $current_dir/themes/jon ~/.config/ghostty/themes/jon
