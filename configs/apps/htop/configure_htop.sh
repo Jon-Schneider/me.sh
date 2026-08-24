20abe52c4516be70aaf4eb0cee0100d1efd5a716
Jon Schneider <jon@jonschneider.me>
2026-08-24 17:23:20 -0400

    Migrate htop config to manifest

--- configs/apps/htop/configure_htop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Configuring htop..."
-mkdir -p ~/.config/htop 2> /dev/null # Redirect stderr to suppress dir already exists log
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -sfn $current_dir/htoprc ~/.config/htop/htoprc
